adf57c6a02286498bcbbb7f315bb1f09406c9d4f
Strapi Application User <strapi@ip-172-31-1-107.eu-central-1.compute.internal>
2022-07-11 13:57:21 +0000

    Project initialization

---
+++ config/middlewares.js
@@ -0,0 +1,35 @@
+module.exports = ({ env }) => ([
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', env('AWS_BUCKET_NAME') + '.s3.' + env('AWS_REGION') + '.amazonaws.com'],
+          'media-src': ["'self'", 'data:', 'blob:', env('AWS_BUCKET_NAME') + '.s3.' + env('AWS_REGION') + '.amazonaws.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  {
+    name: "strapi::body",
+    config: {
+      formLimit: "256mb", // modify form body
+      jsonLimit: "256mb", // modify JSON body
+      textLimit: "256mb", // modify text body
+      formidable: {
+        maxFileSize: 10 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
+      },
+    },
+  },
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+]);
